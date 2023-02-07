83ead9a416e394f9e68a468b77077b91304aa2cb
Vatsal Jain <vatsal@dhcp-206-12-182-184.ubcsecure.wireless.ubc.ca>
2023-02-07 10:00:27 -0800

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,52 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+// require("./tasks/block-number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    // solidity: "0.8.17",
+    solidity: {
+        compilers: [{ version: "0.8.17" }, { version: "0.6.6" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user: {
+            default: 1,
+        },
+    },
+}
